c991d75993efd1e9c095570030e822fa7dad211a
vjenacka <edins613@gmail.com>
2019-12-08 00:16:21 +0100

    added addsmoothie, addIngredient and deleteIng functionality to the addSmoothie comp.

--- src/components/AddSmoothie.vue
+++ src/components/AddSmoothie.vue
@@ -6,18 +6,21 @@
         <label for="title">Smoothie Title:</label>
         <input type="text" name="title" v-model="title" />
       </div>
-      <div v-for="(ing,i) in ingredients" :key="i">
+      <div v-for="(ing,i) in ingredients" :key="i" class="field">
         <label for="ingredient">Ingredient:</label>
         <input type="text" name="ingredient" v-model="ingredients[i]" />
+        <i class="material-icons delete" @click="deleteIng(ing)">delete</i>
       </div>
       <div class="field add-ingredient">
-        <label for="add-ingredient">Add an ingredient (Tab):</label>
+        <label for="add-ingredient">Add an ingredient:</label>
         <input
           type="text"
           name="add-ingredient"
-          @keydown.tab.prevent="addIngredient"
           v-model="another"
         />
+        <div class="btn-floating btn-small halfway-fab pink darken-2 add" @click="addIngredient">
+          <i class="material-icons">add</i>
+        </div>
       </div>
       <p v-if="feedback" class="red-text center">{{feedback}}</p>
       <div class="field center-align">
@@ -28,6 +31,8 @@
 </template>
 
 <script>
+import db from "../firebase/init";
+import slugify from "slugify";
 export default {
   name: "AddSmoothie",
   data() {
@@ -40,7 +45,29 @@ export default {
   },
   methods: {
     addSmoothie() {
-      console.log(this.title, this.ingredients);
+      if (this.title && this.ingredients.length) {
+        this.feedback = "";
+        //pretvaramo naslovu u slug radi route-anja
+        this.slug = slugify(this.title, {
+          replacement: "-",
+          remove: /[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi,
+          lower: true
+        });
+        db.collection("smoothies")
+          .add({
+            title: this.title,
+            ingredients: this.ingredients,
+            slug: this.slug
+          })
+          .then(() => {
+            this.$router.push({ name: "Index" });
+          })
+          .catch(err => {
+            console.log(err);
+          });
+      } else {
+        this.feedback = "You need to enter a smoothie title and ingredients";
+      }
     },
     addIngredient() {
       if (this.another) {
@@ -50,6 +77,11 @@ export default {
       } else {
         this.feedback = "You must enter a value to add and ingredient";
       }
+    },
+    deleteIng(ing) {
+      this.ingredients = this.ingredients.filter(
+        ingredient => ingredient !== ing
+      );
     }
   }
 };
@@ -68,6 +100,23 @@ export default {
 
   .field {
     margin: 20px auto;
+    position: relative;
+  }
+
+  .delete {
+    position: absolute;
+    right: 0;
+    bottom: 16px;
+    color: #aaa;
+    font-size: 1.4em;
+    cursor: pointer;
+  }
+
+  .add {
+    position: absolute;
+    right: 0;
+    bottom: 14px;
+    cursor: pointer;
   }
 }
 </style>
